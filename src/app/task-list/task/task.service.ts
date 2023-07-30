18c4863402aeb3469dbe217aac8da5f7ab9e58ca
NazariiIhnat <ignatnazar@gmail.com>
2023-07-30 11:24:02 +0200

    All tasks store in task service. All components will recive tasks updates through tasks subject

--- src/app/task-list/task/task.service.ts
+++ src/app/task-list/task/task.service.ts
@@ -6,9 +6,10 @@ import { AuthService } from 'src/app/auth/auth.service';
 
 @Injectable({ providedIn: 'root' })
 export class TaskService implements OnDestroy {
+  userTasks = [];
+  tasksSubject = new Subject<any[]>();
   private apiUrl =
     'https://todo-list-app-58503-default-rtdb.europe-west1.firebasedatabase.app/task/';
-  newTaskSubject = new Subject<[string, Task]>();
   userSubscription = new Subscription();
   userId: string;
 
@@ -36,29 +37,44 @@ export class TaskService implements OnDestroy {
         isImportant: task.isImportant,
         isDone: task.isDone,
       })
-      .subscribe((val) =>
-        this.newTaskSubject.next([val.name.toString(), task])
-      );
+      .subscribe((val) => {
+        this.userTasks.push([val.name.toString(), task]);
+        this.tasksSubject.next(this.userTasks);
+      });
   }
 
   fetch() {
-    return this.http.get<{ string: { task: Task } }>(
-      this.apiUrl + this.userId + '.json'
-    );
+    this.http
+      .get<{ string: { task: Task } }>(this.apiUrl + this.userId + '.json')
+      .subscribe((val) => {
+        if (!val) return;
+        this.userTasks = Object.entries(val);
+        this.tasksSubject.next(this.userTasks);
+      });
   }
 
   delete(taskId: string) {
-    return this.http.delete(this.apiUrl + this.userId + '/' + taskId + '.json');
+    this.http
+      .delete(this.apiUrl + this.userId + '/' + taskId + '.json')
+      .subscribe(() => {
+        this.userTasks = this.userTasks.filter((el) => el[0] !== taskId);
+        this.tasksSubject.next(this.userTasks);
+      });
   }
 
   update(taskId: string, task: Task) {
-    return this.http.put(this.apiUrl + this.userId + '/' + taskId + '.json', {
-      title: task.title,
-      description: task.description,
-      date: task.date,
-      category: task.category,
-      isImportant: task.isImportant,
-      isDone: task.isDone,
-    });
+    this.http
+      .put(this.apiUrl + this.userId + '/' + taskId + '.json', {
+        title: task.title,
+        description: task.description,
+        date: task.date,
+        category: task.category,
+        isImportant: task.isImportant,
+        isDone: task.isDone,
+      })
+      .subscribe(() => {
+        this.userTasks.find((val) => val[0] === taskId)[1] = task;
+        this.tasksSubject.next(this.userTasks);
+      });
   }
 }

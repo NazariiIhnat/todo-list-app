73338e794c032d403768fc309df51231eb1ed17b
NazariiIhnat <ignatnazar@gmail.com>
2023-08-05 13:02:15 +0200

    Title show selected category

--- src/app/headers/headers.component.ts
+++ src/app/headers/headers.component.ts
@@ -1,17 +1,40 @@
-import { Component, EventEmitter, Output } from '@angular/core';
+import {
+  Component,
+  EventEmitter,
+  OnDestroy,
+  OnInit,
+  Output,
+} from '@angular/core';
 import { AuthService } from '../auth/auth.service';
 import { ModalService } from '../modal/modal.service';
+import { CategorySelectionService } from '../category/category-selection.service';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-headers',
   templateUrl: './headers.component.html',
   styleUrls: ['./headers.component.css'],
 })
-export class HeadersComponent {
+export class HeadersComponent implements OnInit, OnDestroy {
+  categorySelectionSubscription = new Subscription();
+  selectedCategory: string;
+
   constructor(
     private authService: AuthService,
-    private modalService: ModalService
+    private modalService: ModalService,
+    private categorySelectionService: CategorySelectionService
   ) {}
+
+  ngOnInit(): void {
+    this.categorySelectionSubscription =
+      this.categorySelectionService.selectedCategorySubject.subscribe(
+        (val) => (this.selectedCategory = val)
+      );
+  }
+
+  ngOnDestroy(): void {
+    this.categorySelectionSubscription.unsubscribe();
+  }
 
   onLogout() {
     this.authService.logout();
